428fd2955003c12407b03d6c9a883e4e91ea7452
Byakuya91 <nganti91@yahoo.com>
2022-04-17 08:19:22 -0400

    I've successfully added a button and it won't reload the page thanks to the onSubmit function. Working now on making a post being able to submit and update.

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 
 const CreatePosts = (props) => {
+
+    // construct a post object that will enter our post array.
+    const [name, setName] = useState('');
+    const [post, setPost] = useState('');
+
+    // creating a function to prevent the page from reloading 
+    function handleSubmit(event){
+        event.preventDefault();
+    }
+
     return ( 
-       <form>
+       <form onSubmit={handleSubmit}> 
        <labeL>Name</labeL> 
-       <input type="text" />  
+       <input type="text" value={name}  onChange ={(event)=> setName(event.target.value) }/>  
        <labeL>Post</labeL> 
-       <input type="text" />
-
-
-
-
+       <input type="text" value={post}onChange ={(event)=> setPost(event.target.value) } />
+       <button type='submit'>create a post </button>
        </form>
 
 
